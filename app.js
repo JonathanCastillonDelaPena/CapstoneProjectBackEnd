687705cc3cb570dd2112c863d3f966f7f7b77f9a
Tristan Viernes <trztn.keith@gmail.com>
2023-03-03 15:01:04 +0800

    first

---
+++ app.js
@@ -0,0 +1,27 @@
+//import modules
+const express = require('express');
+const users = require('./routes/users');
+const sql = require('./database/mySQL');
+require('dotenv/config');
+
+//middleware
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true,}));
+//routes
+app.use('/', users);
+
+//check connection of database and server if running
+
+
+sql.connect((err)=>{
+    if(!err){
+        console.log('Database Connected')
+        const port = process.env.PORT || 3001;
+        app.listen(port, ()=>{
+            console.log(`Running in port ${port}`);
+        });
+     } else { 
+        console.log('Failed Connection Due:', JSON.stringify(err.sqlMessage)) 
+    }
+});
