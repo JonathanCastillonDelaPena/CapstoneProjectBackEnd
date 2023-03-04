348a4ff9fdb9d570a31c0f4ea554e8962e122123
Jonathan C Dela Peña <jonathancastillondelapena28@gmail.com>
2023-03-04 14:36:23 +0800

    Added cors middleware

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const express = require("express");
 const users = require("./routes/users");
 const posts = require("./routes/post");
 const sql = require("./database/mySQL");
+const cors = require("cors");
 require("dotenv/config");
 
 //middleware
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //routes
