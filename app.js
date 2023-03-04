b44c3c95d060b228cc2a43158c834bf5fddbe634
Jonathan C Dela Peña <jonathancastillondelapena28@gmail.com>
2023-03-04 09:13:41 +0800

    Implemented Basic CRUD of Post Table

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
 //import modules
-const express = require('express');
-const users = require('./routes/users');
-const sql = require('./database/mySQL');
-require('dotenv/config');
+const express = require("express");
+const users = require("./routes/users");
+const posts = require("./routes/post");
+const sql = require("./database/mySQL");
+require("dotenv/config");
 
 //middleware
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended: true,}));
+app.use(express.urlencoded({ extended: true }));
 //routes
-app.use('/', users);
+app.use("/", users);
+app.use("/", posts);
 
 //check connection of database and server if running
 
-
-sql.connect((err)=>{
-    if(!err){
-        console.log('Database Connected')
-        const port = process.env.PORT || 3001;
-        app.listen(port, ()=>{
-            console.log(`Running in port ${port}`);
-        });
-     } else { 
-        console.log('Failed Connection Due:', JSON.stringify(err.sqlMessage)) 
-    }
+sql.connect((err) => {
+  if (!err) {
+    console.log("Database Connected");
+    const port = process.env.PORT || 3001;
+    app.listen(port, () => {
+      console.log(`Running in port ${port}`);
+    });
+  } else {
+    console.log("Failed Connection Due:", JSON.stringify(err.sqlMessage));
+  }
 });
---
+++ routes/post.js
@@ -0,0 +1,70 @@
+const routesPost = require("express").Router();
+const sql = require("../database/mySQL");
+
+routesPost.get("/post/", (req, res) => {
+  try {
+    sql.query("CALL ShowAllPost()", (err, rows) => {
+      res.status(201).send(rows[0]);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+routesPost.get("/post/:id", (req, res) => {
+  const id = req.params.id;
+  try {
+    sql.query(`CALL ShowPostByID(${id})`, (err, rows) => {
+      res.status(201).send(rows[0]);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+routesPost.post("/post/editPost", (req, res) => {
+  const post_id = parseInt(req.body.postId);
+  const title = req.body.postTitle;
+  const content = req.body.postContent;
+
+  try {
+    sql.query(
+      `CALL UpdatePost(${post_id}, '${title}', '${content}')`,
+      (err, rows) => {
+        res.status(201).send(rows);
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+routesPost.post("/post/addPost", (req, res) => {
+  const user_id = parseInt(req.body.userId);
+  const title = req.body.postTitle;
+  const content = req.body.postContent;
+
+  try {
+    sql.query(
+      `CALL AddPost(${user_id}, '${title}', '${content}')`,
+      (err, rows) => {
+        res.status(201).send(rows);
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+routesPost.post("/post/deletePost", (req, res) => {
+  const post_id = req.body.postId;
+  try {
+    sql.query(`CALL DeletePostByID(${post_id})`, (err, rows) => {
+      res.status(201).send(rows[0]);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+module.exports = routesPost;
